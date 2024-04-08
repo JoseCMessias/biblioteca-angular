00aff597bf40e0865db7c4e98007e04796dd0563
José Messias <josecmessias22@gmail.com>
2024-04-08 09:23:30 -0300

    Desafio tres

---
+++ Trilha_Desafio_3/model/Produto.js
@@ -0,0 +1,8 @@
+export default class Produto {
+    constructor(id, nome, preco, quantidade){
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
